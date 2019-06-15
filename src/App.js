5a56630f0cd73a6537dd86cb6ae4dfadcc591285
Nien <wolfzxcv@gmail.com>
2019-06-15 20:43:08 +0100

    test fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-function App() {
+const App = ({className}) => {
+  const [data, setData] = useState([])  
+  const [fylkeNr, setFylkeNr] = useState('')
+
+  useEffect( () =>{      
+    async function fetchData(){
+    const response = await fetch('https://data-nsr.udir.no/enheter')
+    const result = await response.json()
+    setData(result)     
+  }
+    fetchData()
+},[])
+
+  console.log(data,'got '+data.length+' data')
+
+  const filterArr = data.filter( x => fylkeNr.includes(x.FylkeNr))
+  console.log(filterArr)
+
+  const filterFylkeNrAmount = data.reduce( (eachData, fylkeNr) => {
+    eachData[fylkeNr.FylkeNr] = 0  
+   return eachData   },{})
+
+  const getFylkeNr = Object.keys(filterFylkeNrAmount).sort( (a,b) => a-b)
+  console.log(getFylkeNr)
+
+  const warning = e =>{
+    setFylkeNr( e.target.value); 
+    if(fylkeNr<51){
+      return null
+    }else{
+      alert('please enter a number between 01-50');
+      setFylkeNr('')}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={className}>
+    <a name="top"></a>
+    <input value={fylkeNr} onChange={ e =>warning(e) } />
     </div>
-  );
+ )
 }
 
-export default App;
+App.propTypes = {
+  className: PropTypes.string
+}
+
+const StyledApp = styled(App)` 
+`
+
+StyledApp.displayName = 'App'
+
+export default StyledApp
