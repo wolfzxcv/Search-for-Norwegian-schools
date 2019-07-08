231781deb45414672aaa7a22aa0f9e75885151c5
Nien <wolfzxcv@gmail.com>
2019-07-08 22:10:25 +0100

    Updates

---
+++ precache-manifest.f47a4a8e30577ed6f0503b8a0a42cdfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3028da1f0e6324343c5c469624ec4475",
+    "url": "/Search-for-Norwegian-schools/index.html"
+  },
+  {
+    "revision": "8e369e753bef6b9f26a5",
+    "url": "/Search-for-Norwegian-schools/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "729b22d4948f69ffba53",
+    "url": "/Search-for-Norwegian-schools/static/js/2.bd05cd80.chunk.js"
+  },
+  {
+    "revision": "8e369e753bef6b9f26a5",
+    "url": "/Search-for-Norwegian-schools/static/js/main.6bef4166.chunk.js"
+  },
+  {
+    "revision": "d59867a6a78e7413d3a9",
+    "url": "/Search-for-Norwegian-schools/static/js/runtime~main.b8d226aa.js"
+  },
+  {
+    "revision": "80b569cde35cbad9b9c5c0d11b2a0e58",
+    "url": "/Search-for-Norwegian-schools/static/media/google.80b569cd.png"
+  }
+]);
